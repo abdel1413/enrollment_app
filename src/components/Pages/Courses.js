210e0586e60b35f0fd35cd2735ec235b17aece56
Aboulaye Tchakoura <aboulayetchakoura@Aboulayes-Air.home>
2021-10-01 01:29:52 -0400

    fix bug

--- src/components/Pages/Courses.js
+++ src/components/Pages/Courses.js
@@ -2,7 +2,9 @@ import React from 'react'
  const Courses =()=>{
    return(
        <div>
-           <h1>This is course</h1>
+           <ul className="courslist">
+               <li></li>
+           </ul>
        </div>
    )
 }
