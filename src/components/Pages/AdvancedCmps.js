d41a9fd7962c1813860e4fb635adbf8ef004e79c
Aboulaye Tchakoura <aboulayetchakoura@Aboulayes-Air.home>
2021-10-28 10:37:18 -0400

    got courses successful display on sreen

--- src/components/Pages/AdvancedCmps.js
+++ src/components/Pages/AdvancedCmps.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './AdvanceCmp.css'
+import axios from 'axios'
 
 const AdvancedCmps =()=>{
+   const [ advCourse, setAdvCourse] = useState([])
+
+   const DisplayCourses =()=>{
+    axios.get('http://localhost:3001/advancedcourses')
+        .then((response)=>{
+        setAdvCourse(response.data)
+          
+     })
+
+   } 
     return(
-        <div>
-            <p className='advancecourses'>
-                this is AdvancedCmps course page
-            </p>
+        <div className='advancecourses'>
+            <button onClick={DisplayCourses}> course list</button>
+            {advCourse.map((value,key)=>{
+                    return(
+                        <div  className='courses' key={key}>
+                            <h3>{value.Classe}</h3>
+                            <h3>{value.Course_Name}</h3>
+                            <h3>{value.Course_Unit}</h3>
+                            
+                        </div>
+                    )
+                })
+                }
+           
+            
         </div>
     )
 }
--- src/components/Pages/Courses.js
+++ src/components/Pages/Courses.js
@@ -1,25 +1,79 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import './Course.css'
+/*
+function Courses (){
+    const [courses, setCourses]= useState(0)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCourses((courses) => courses + 2); 
+        }, 2000);
+    }, []);
+    return  <h1> {courses} </h1>
+}*/
 
 
- const Courses =()=>{
-     const [courses,setCourses]= useState([])
+ const  Courses =  ()=>{
+     const [courses, setCourses]= useState([])
+    //const data = [1,2,3,4,5]
+     //setCourses(data)
 
-     const getCourses =()=>{
-       const courseArray=  axios.get("http://localhost:3001/courselist").then((response)=>{
-             setCourses(response)
-             console.log(courseArray)
-         })
-        
-     }
+     useEffect(() => {
+       const  getCoursesData = async ()=>{
+
+        const response =  await axios.get("http://localhost:3001/courselist");
+             console.log("Still testing")
+            //  console.log(response.data)
+             setCourses(response.data)
+
+         }
+         getCoursesData();
+
+     }, []);
+
+     console.log(courses)
+
+
+
+    //const response = await axios.get("http://localhost:3001/courselist");
+    //console.log(response.data)
+    //setCourses({courses: response.data});
+    //console.log(courses)
+       //const displayCoursesHandler = ()=>{ 
+           //console.log("inside the functuion;")
+        /*axios.get("http://localhost:3001/courselist")
+                                        .then((response)=>{
+                                        setCourses(response.data).
+                                        catch(err => {
+                                            console.log(err);
+                                            return null;
+                                        });
+        //console.log("after the the axios");
+              }  ) } */
+     //   console.log( displayCoursesHandler )                  
+        // console.log(courses)
+        // console.log("test");
    return(
-       <div className="courslist">
+       <div className="generalcourse">
            
-            <p className='courses'> course List </p> 
-            <p className='courses'>{getCourses } </p> 
+            <p>testing </p>
+
+            {
+                courses.map((val,i)=>{
+                    return(
+                        <div className='courslist' key={i}>
+                           <h3>{val.Class}</h3> 
+                           <h3>{val.Name}</h3> 
+                           <h3>{val.Unit}</h3> 
+                        </div>
+                    )
+                })               
+            }
+            
+            
        </div>
    )
 }
 
-export default Courses
+ export default Courses
