138901321ea7ea63b271aa32f4d093406be9c1dc
Aboulaye Tchakoura <aboulayetchakoura@Aboulayes-Air.home>
2021-11-01 10:13:58 -0400

    addedd sig up

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -4,6 +4,7 @@ import  "./Home.css";
 import axios from "axios";
 import SignIn from "../signInComponent/SignIn";
 import SignOut from "../SignOutComp/SignOut";
+import SignUp from "./SignUp";
 
 class Home extends React.Component{
   constructor(){
@@ -31,7 +32,6 @@ class Home extends React.Component{
 
 //need to signin before you can see home screen
  onRouteChange=(route)=>{
-
 this.setState({route})
   }
   
@@ -47,41 +47,46 @@ this.setState({route})
             color: 'green'
             }}>
              <SignOut onRouteChange={this.onRouteChange}/>
-              {this.state.route ==='signin'
-              ?<SignIn onRouteChange ={this.onRouteChange}/>
-              :<div>
-                  <h1>Welcom to quick enrollment site</h1>
-                  <h1>This app is designed to help student
-                  <br/>easily and quickly
-                    register for their classes <br/>
-                  </h1>
-                  <div style={{display:'flex',
-                      flexDirection:'column', 
-                      fontSize:'25px',
-                      padding:'20px',marginTop:'20px'}}>
-                      
-                      <label>Classname</label>
-                        <input 
-                          type="text" 
-                          onChange={(e)=>{this.setState({classname: e.target.value})}}
-                          placeholder=' Enter class'/>
+              {this.state.route ==='home'
+              ?<div>
+                <h1>Welcom to quick enrollment site</h1>
+                <h1>This app is designed to help student
+                <br/>easily and quickly
+                  register for their classes <br/>
+                </h1>
+                <div style={{display:'flex',
+                    flexDirection:'column', 
+                    fontSize:'25px',
+                    padding:'20px',marginTop:'20px'}}>
                     
-                       <label>Name </label>
-                         <input 
-                            type="text" 
-                            onChange={(e)=>{this.setState({Name: e.target.value})}}
-                            placeholder=' Enter Name'/>
-                      
-                       <label>Unit</label>
-                        <input 
-                          type="number" 
-                            onChange={(e)=>{this.setState({Unit: e.target.value})}} 
-                            placeholder= 'Enter Unit'
-                          />
-                        <br/>
-                     <button onClick={this.Addclass}> add class</button>
-                 </div>
-         </div> }
+                    <label>Classname</label>
+                      <input 
+                        type="text" 
+                        onChange={(e)=>{this.setState({classname: e.target.value})}}
+                        placeholder=' Enter class'/>
+                  
+                    <label>Name </label>
+                      <input 
+                          type="text" 
+                          onChange={(e)=>{this.setState({Name: e.target.value})}}
+                          placeholder=' Enter Name'/>
+                    
+                    <label>Unit</label>
+                      <input 
+                        type="number" 
+                          onChange={(e)=>{this.setState({Unit: e.target.value})}} 
+                          placeholder= 'Enter Unit'
+                        />
+                      <br/>
+                    <button onClick={this.Addclass}> add class</button>
+                </div>
+                </div> 
+                
+              :( this.state.route==='signin'
+                  ? <SignIn onRouteChange={this.onRouteChange}/>
+                  : <SignUp onRouteChange={this.onRouteChange}/>
+                )
+              }
       </div>
     );
           }
--- src/components/Pages/SignUp.js
+++ src/components/Pages/SignUp.js
@@ -1,17 +1,76 @@
-import React, { useState } from 'react'
+import React from 'react'
+import 'tachyons'
 
+const SignUp =({onRouteChange})=>{
 
-const SignUp =()=>{
-  
-    return (
-        <div style={{
-            display:'flex',
-            justifyContent:'right',
-            alignItems:'right',
-            height:'100vh'
-        }}>
+    return(
        
-        </div>
+       
+        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80 ">
+            <form className="measure ">
+              <fieldset id="sign_up" 
+                    className="ba b--transparent ph0 mh0">
+                <legend className="f1 fw6 ph0 mh0 center">Register</legend>
+                <div className="mt3">
+                    <label 
+                        className="db fw6 lh-copy f6" 
+                        for="firstname">First Name
+                    </label>
+                    <input 
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                    type="name" 
+                    name="name" 
+                    id="name"/>
+                </div>
+
+                <div className="mt3">
+                    <label 
+                        className="db fw6 lh-copy f6" 
+                        for="lastname">Last Name
+                    </label>
+                    <input 
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                    type="lastname" 
+                    name="lastname" 
+                    id="lastname"/>
+                </div>
+                  <div className="mt3">
+                    <label 
+                        className="db fw6 lh-copy f6" 
+                        for="email-address">Email
+                    </label>
+                    <input 
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                    type="email" 
+                    name="email-address" 
+                    id="email-address"/>
+                </div>
+                <div className="mv3">
+                    <label 
+                        className="db fw6 lh-copy f6" 
+                        for="password">Password
+                    </label>
+                    <input 
+                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                    type="password" 
+                    name="password"  
+                    id="password"/>
+     
+                </div>
+                
+                </fieldset>
+                
+                <div className="lh-copy mt3 pointer">
+                    <p onClick={()=>{onRouteChange('signin')}}
+                    className="f6 link dim black db center ml10">Register</p>
+            
+                </div>
+            </form>
+          </main>
+        </article>
+        //   </div> 
     )
 }
+
 export default SignUp;
